864533b044dbdc6e2b8542dbc081b003c9c28dc8
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-06-17 00:23:07 +0900

    Comment out SUPABASE env references

--- apps/readest-app/src/utils/supabase.ts
+++ apps/readest-app/src/utils/supabase.ts
@@ -1,9 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
 
+/* SUPABASE_DISABLED
 const supabaseUrl =
   process.env['NEXT_PUBLIC_SUPABASE_URL'] || process.env['NEXT_PUBLIC_DEV_SUPABASE_URL']!;
 const supabaseAnonKey =
   process.env['NEXT_PUBLIC_SUPABASE_ANON_KEY'] || process.env['NEXT_PUBLIC_DEV_SUPABASE_ANON_KEY']!;
+*/
+
+const supabaseUrl = '';
+const supabaseAnonKey = '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
