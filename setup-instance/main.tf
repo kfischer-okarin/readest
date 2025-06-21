dba7aec3b7f12a40f4a5360a12a804cca5e68709
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-06-21 18:02:08 +0900

    Replace build instance script with terraform

---
+++ setup-instance/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.40"
+    }
+  }
+}
+
+provider "google" {}
+
+variable "instance_name" {
+  description = "Name of the compute instance"
+  type        = string
+}
+
+resource "google_service_account" "default" {
+  account_id   = "app-instance-sa"
+  display_name = "App Instance SA"
+}
+
+resource "google_compute_instance" "app_instance" {
+  name         = var.instance_name
+  machine_type = "e2-standard-2"
+  allow_stopping_for_update = true
+
+  service_account {
+    email = google_service_account.default.email
+    scopes = [
+      "cloud-platform"
+    ]
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+}
